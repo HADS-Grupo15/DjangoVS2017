4d3ed6159866355fb8623cd2126b5c8eb357b3ed
HADS-Grupo15 <opalomo001@ikasle.ehu.es>
2018-05-10 12:38:18 +0200

    Punto 1
    Aun hay errores

---
+++ DjangoWebProjectVS2017/DjangoWebProjectVS2017/app/models.py
@@ -0,0 +1,31 @@
+"""
+AQUI VAN LAS "CLASES"
+"""
+
+from django.db import models
+from django.db.models.fields import CharField,BooleanField
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class QuizQuestion(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    topic = models.CharField(max_length=200)
+
+class QuizChoice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    correct = models.BooleanField
+
+class User(models.Model):
+    email = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
---
+++ DjangoWebProjectVS2017/DjangoWebProjectVS2017/app/views.py
@@ -0,0 +1,193 @@
+"""
+AQUI VAN LAS FUNCIONES 
+"""
+
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+from django.http.response import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import QuizQuestion,QuizChoice,Question,Choice,User
+from django.template import loader
+from django.core.urlresolvers import reverse
+from app.forms import QuizQuestionForm,QuizChoiceForm,QuestionForm, ChoiceForm,UserForm
+from django.shortcuts import redirect
+import json
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Autor de la web',
+            'message':'Datos de contacto',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')
+    template = loader.get_template('polls/index.html')
+    context = {
+                'title':'Lista de preguntas de la encuesta',
+                'latest_question_list': latest_question_list,
+              }
+    return render(request, 'polls/index.html', context)
+
+def Quiz_index(request):
+    latest_question_list = QuizQuestion.objects.order_by('-pub_date')
+    template = loader.get_template('quiz/index.html')
+    context = {
+                'title':'Lista de preguntas de la encuesta',
+                'latest_question_list': latest_question_list,
+              }
+    return render(request, 'quiz/index.html', context)
+
+def detail(request, question_id):
+     question = get_object_or_404(Question, pk=question_id)
+     #Respuestas asociadas a la pregunta es un String que se colocará en el tag del title, y en el tag de question el valor de question#
+     return render(request, 'polls/detail.html', {'title':'Respuestas asociadas a la pregunta:','question': question})
+
+def QuizDetail(request, question_id):
+     question = get_object_or_404(QuizQuestion, pk=question_id)
+     #Respuestas asociadas a la pregunta es un String que se colocará en el tag del title, y en el tag de question el valor de question#
+     return render(request, 'quiz/detail.html', {'title':'Respuestas asociadas a la pregunta:','question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'title':'Resultados de la pregunta:','question': question})
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Vuelve a mostrar el form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "ERROR: No se ha seleccionado una opcion",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Siempre devolver un HttpResponseRedirect despues de procesar
+        # exitosamente el POST de un form. Esto evita que los datos se
+        # puedan postear dos veces si el usuario vuelve atras en su browser.
+        return HttpResponseRedirect(reverse('results', args=(p.id,)))
+
+def choice_add(request, question_id):
+        question = Question.objects.get(id = question_id)
+        if request.method =='POST':
+            form = ChoiceForm(request.POST)
+            if form.is_valid():
+                choice = form.save(commit = False)
+                choice.question = question
+                choice.vote = 0
+                choice.save()         
+                #form.save()
+        else: 
+            form = ChoiceForm()
+        #return render_to_response ('choice_new.html', {'form': form, 'poll_id': poll_id,}, context_instance = RequestContext(request),)
+        return render(request, 'polls/choice_new.html', {'title':'Pregunta:'+ question.question_text,'form': form})
+
+def question_new(request):
+        if request.method == "POST":
+            form = QuestionForm(request.POST)
+            if form.is_valid():
+                question = form.save(commit=False)
+                question.pub_date=datetime.now()
+                question.save()
+                #return redirect('detail', pk=question_id)
+                #return render(request, 'polls/index.html', {'title':'Respuestas posibles','question': question})
+        else:
+            form = QuestionForm()
+        return render(request, 'polls/question_new.html', {'form': form})
+
+def Quiz_question_new(request):
+        if request.method == "POST":
+            form = QuizQuestionForm(request.POST)
+            if form.is_valid():
+                question = form.save(commit=False)
+                question.pub_date=datetime.now()
+                question.save()
+                #return redirect('detail', pk=question_id)
+                return render(request, 'quiz/index.html', {'title':'Ha funcionado','question': question})
+        else:
+            form = QuizQuestionForm()
+            return render(request, 'quiz/index.html', {'tile':'Ha funcioaao','form': form})
+
+def Quiz_choice_add(request, question_id):
+        question = QuizQuestion.objects.get(id = question_id)
+        if request.method =='POST':
+            form = QuizChoiceForm(request.POST)
+            if form.is_valid():
+                choice = form.save(commit = False)
+                choice.question = question
+                choice.vote = 0
+                choice.save()         
+                #form.save()
+        else: 
+            form = QuizChoiceForm()
+        #return render_to_response ('choice_new.html', {'form': form, 'poll_id': poll_id,}, context_instance = RequestContext(request),)
+        return render(request, 'quiz/choice_new.html', {'title':'Pregunta:'+ question.question_text,'topic':'Tema: '+question.topic,'form': form})#añadirle el flag(?)#
+
+def chart(request, question_id):
+    q=Question.objects.get(id = question_id)
+    qs = Choice.objects.filter(question=q)
+    dates = [obj.choice_text for obj in qs]
+    counts = [obj.votes for obj in qs]
+    context = {
+        'dates': json.dumps(dates),
+        'counts': json.dumps(counts),
+    }
+
+    return render(request, 'polls/grafico.html', context)
+
+def user_new(request):
+        if request.method == "POST":
+            form = UserForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.save()
+                #return redirect('detail', pk=question_id)
+                #return render(request, 'polls/index.html', {'title':'Respuestas posibles','question': question})
+        else:
+            form = UserForm()
+        return render(request, 'polls/user_new.html', {'form': form})
+
+def users_detail(request):
+    latest_user_list = User.objects.order_by('email')
+    template = loader.get_template('polls/users.html')
+    context = {
+                'title':'Lista de usuarios',
+                'latest_user_list': latest_user_list,
+              }
+    return render(request, 'polls/users.html', context)
